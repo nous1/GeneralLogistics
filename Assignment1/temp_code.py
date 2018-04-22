b49aa63c26d132d144f43bfc65a828a87bf454c0
Cristopher Rosales <ichneutae@gmail.com>
2018-04-22 18:59:07 -0300

    Update temp_code.py

--- Assignment1/temp_code.py
+++ Assignment1/temp_code.py
@@ -26,34 +26,46 @@ dataset = pandas.read_excel(url, names=names, float_format='%.f')
 # Console size
 pandas.set_option('display.height', 2000)
 pandas.set_option('display.max_rows', 500)
-pandas.set_option('display.max_columns', 500)
-pandas.set_option('display.width', 2000)
+pandas.set_option('display.max_columns', 50)
+pandas.set_option('display.width', 1300)
 
-
-
+#==============================================================================
 # print(dataset['numero_cuotas'].describe())
 # print(dataset.dtypes) -- TIPOS DE DATOS
+#==============================================================================
 
+#==============================================================================
+# GRAFICAR
+# fig, ax = plt.subplots()
+# dataset.plot.scatter(x='ID',y='NIV_EDUC_ENCODED',s=1, color='red', label='Credito_2')
+# ax.ticklabel_format(style='plain')
+# plt.show()
+#==============================================================================
 
-
-
-#fig, ax = plt.subplots()
-#dataset.plot.scatter(x='ID',y='Credito_4',s=1, color='red', label='RENTA')
-#ax.ticklabel_format(style='plain')
-#plt.show()
-
-# Transformar columnas nominales a numericas
+# Transformar columnas nominales
 dataset['NIV_EDUC_ENCODED'] = LabelEncoder().fit_transform(dataset['NIV_EDUC']) 
 dataset['GENERO_ENCODED'] = dataset['GENERO'].map({'F       ':0,'M       ':1})
 dataset['E_CIVIL_ENCODED']  = LabelEncoder().fit_transform(dataset['E_CIVIL'])
-dataset['CIUDAD_ENCODED'] = LabelEncoder().fit_transform(dataset['CIUDAD'])
+
+# ----PENDIENTE------
+# HOT ONE Encoding para ciudades (78 ciudades)
+# print(pandas.get_dummies(dataset['CIUDAD'], prefix='CIUDAD').head(1))
+# --------------------
+
+# Mostrar valores unicos de NIV_EDUC
+#print(dataset.NIV_EDUC.unique())
+
 # Eliminar columnas originales
 del dataset['GENERO']
 del dataset['NIV_EDUC']
 del dataset['E_CIVIL']
 del dataset['CIUDAD']
-# Eliminar todas las filas donde Monto_solicitado sea cero.
+
+# Eliminar todas las filas donde Monto_solicitado sea cero. (406 Filas)
 dataset = dataset.drop(dataset[dataset['Monto_solicitado']==0].index)
+# Eliminar todas las filas donde Credito_2 sea negativo. (52 filas)
+dataset = dataset.drop(dataset[dataset['Credito_2']<0].index)
+
 
 
 
